7007a6bfce4ee73f356267e30cd01927c66fdcb1
Mateo Ceron <mateo-ceron@hotmail.fr>
2018-08-13 17:29:37 +0200

    exo 2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,18 +9,18 @@ class GamesController < ApplicationController
     require 'json'
     require 'open-uri'
     @word = params[:word]
-    @sum = 0
+
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     user_words = open(url).read
     user = JSON.parse(user_words)
     @letters = params[:letters].split(' ')
     if user['found']
-      @sum += 1
+      @session_score = session[:score].to_i
+      @session_score += 1
       return @result = "Congratulations! #{@word} is a valid English word!"
     end
-    return @sum
     @word.split.each do |lettre|
-      if @letters.include?(lettre)
+      if @letters.include?(lettre.downcase)
         return @result = "Sorry but #{@word} can't be built out of #{@letters}"
       else
         return @result = "Sorry but #{@word} does not to be a valid English Word"
